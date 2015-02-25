1cee03603aabc57d5088dd535bfb9bb36b4ceb21
geofholbrook <geof.holbrook@gmail.com>
2015-02-25 15:39:36 +0100

    add arrangement species

--- darwin-sources/dwn.defspecies-demo.lisp
+++ darwin-sources/dwn.defspecies-demo.lisp
@@ -41,39 +41,62 @@
   )
 
 |#
-              
-(defspecies melody ()
-            
-            :species-slots
-            (range '(60 72))
-            
-            :operon-slots
-            (pitch :range (range self))
-            
-            :phenotyper
-            (make-even-melody (mapcar 'pitch (operons self))
-                              1/16))
+        
+(defspecies arrangement (specimen)
+
+  :species-slots
+  (range '(60 72))
+  (resolution 1/16)
+  (extent 2)        ;in whole notes
+  (length-range '(1 16))  ;in whole notes
+  (channel-range '(1 4))
+
+  :operon-slots
+  (start :range (list 0 (extent self) (resolution self)))
+  (len :range `(,@(length-range self) ,(resolution self)))
+  (channel :range (channel-range self))
+  (pitch  :range (range self))
+
+  :phenotyper
+  (loop for op in (operons self)
+        collect (make-region (start op) (len op) (channel op) (pitch op))))
+  
+      
+(defmethod finalize ((self arrangement))
+  (arrange->poly (phenotype self)))
+
+
+(defspecies melody (arrangement)         
+  :species-slots (range '(60 72))
+  :operon-slots (pitch :range (range self))           
+  :phenotyper (make-even-melody (mapcar 'pitch (operons self))
+                     1/16))
 
 ;test
 ;(setf m1 (make-instnace 'melody))
 ;(run m1 #'(lambda (m) (count-if 'evenp (mapcar 'pitch (operons m)))) 10 :finalizer #'(lambda (s) (mapcar 'pitch (operons s))))
 
-(defmethod finalize ((self melody))
-  (arrange->poly (phenotype self)))
 
-(defspecies dx-melody (melody)
+(defspecies dx-melody (arrangement)
             
-            :species-slots
-            (start 48)
-            (melodic-range '(-3 3))
+  :species-slots
+  (start 48)
+  (melodic-range '(-3 3))
 
-            :operon-slots
-            (interval :range (melodic-range self))
+  :operon-slots
+  (interval :range (melodic-range self))
 
-            :phenotyper
-            (make-even-melody (dx->x (start self) 
-                                     (mapcar #'interval (operons self)))
-                              1/16))
+  :phenotyper
+  (make-even-melody (dx->x (start self) 
+                           (mapcar #'interval (operons self)))
+                    1/16))
+
+
+;TODO
+
+;(defspecies phrases (melody)
+;  )
+  
 
 
 
