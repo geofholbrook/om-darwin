01fa90586062c0d4d1950e3ea8a799d3f1ae2d24
Geof Holbrook <geof.holbrook@gmail.com>
2015-05-12 15:36:23 +0200

    add get-overlap, empty shell function to be filled

--- darwin-sources/dwn.maquette.lisp
+++ darwin-sources/dwn.maquette.lisp
@@ -42,21 +42,6 @@
      
      (otherwise (call-next-method))))
 
-;;; not necessary?
-(defmethod d::get-tempobjs ((self relationPanel))
-  (remove-if-not #'(lambda (icon)
-                     (subtypep (type-of icon) 'tempobjframe))
-                 (om-subviews self)))
-
-(defmethod find-connected-ga-box ((self tempobjframe))
-  ;;; finds the first box connected to the tempout inside the represented patch
-  (let ((connected-box (first (connected? 
-                         (first (inputs (find-if #'(lambda (b)
-                                                     (equalp (type-of b) 'omtempout))
-                                                 (boxes (reference (object self))))))))))
-    (when (d::is-ga-box-p connected-box)
-      connected-box)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmethod maq-toggle-evolution ((self MaquettePanel))
   (let ((boxes 
@@ -76,23 +61,5 @@
 )
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;; not necessary?
+(defmethod! get-overlap () ())
 
-(defmethod is-editorwindow ((self editorwindow)) t)
-(defmethod is-editorwindow ((self t)) nil)
-
-(defun engine-in-front-maquette (engine)
-  (let ((front (om-front-window)))
-    (when (is-editorwindow front)
-      (when (equalp (type-of (obj front)) 'ommaquette)
-        (let ((box (find engine
-                         (boxes (object (editorframe (obj front))))
-                         :key #'(lambda (b) (first (value b))))))
-          (when box
-            (let ((frame (first (frames box))))
-              (redraw-frame frame)
-             ; (when (om-view-container frame)
-             ;  (omg-select frame))
-            )))))))
-  
