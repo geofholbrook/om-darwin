62022b8022f3139ad39ddf31e2022c6daabda248
Geof Holbrook <geof.holbrook@gmail.com>
2015-06-19 18:09:03 +0200

    update ws

--- cbsolo-presentation/wsparams.lisp
+++ cbsolo-presentation/wsparams.lisp
@@ -1,1 +1,1 @@
-(in-package :om)(setf *ws-params* (list 1 (om-make-point 922 100) (om-make-point 413 346)))
+(in-package :om)(setf *ws-params* (list 1 (om-make-point 430 78) (om-make-point 413 346)))
