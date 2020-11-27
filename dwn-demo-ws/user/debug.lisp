4bf58fc6b561c62c61539eb88fd7011e7a6e8dd0
Geof Holbrook <geof.holbrook@gmail.com>
2020-11-26 22:15:35 -0500

    disable ws user code that requires dwn

--- dwn-demo-ws/user/debug.lisp
+++
@@ -1,8 +0,0 @@
-(in-package om)
-
-
-
-(defun fooo ()
-  (make-stack (list (define-species #'(lambda ()
-                                        (d::make-even-melody (om* (om+ (om-gene 50 61) '(0 4 7 12)) 100) 1/16)))
-                    (make-melody 4 '(6000 7200)))))
--- dwn-demo-ws/user/new-arrangers.lisp
+++ dwn-demo-ws/user/new-arrangers.lisp
@@ -1,14 +1,14 @@
-(in-package dwn)
+;; (in-package dwn)
 
 
-(defspecies trills (arrangement)
-  :phenotyper   ;;; change only the pheno
+;; (defspecies trills (arrangement)
+;;   :phenotyper   ;;; change only the pheno
 
-  (loop for op in (operons self)
-        append (loop for start from (start op) by (resolution self)
-                     repeat (floor (len op) (resolution self))
-                     for k from 0
-                     collect (make-region start (resolution self)
-                                          (channel op)
-                                          (+ (pitch op) (if (evenp k)
-                                                            0 100))))))
+;;   (loop for op in (operons self)
+;;         append (loop for start from (start op) by (resolution self)
+;;                      repeat (floor (len op) (resolution self))
+;;                      for k from 0
+;;                      collect (make-region start (resolution self)
+;;                                           (channel op)
+;;                                           (+ (pitch op) (if (evenp k)
+;;                                                             0 100))))))
